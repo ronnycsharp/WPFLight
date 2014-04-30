795ac26e6af00833d08af502f6830231b94bd650
RonnyCSHARP <ronny.weidemann@live.com>
2014-04-30 09:02:24 +0200

    Files included ;)

---
+++ WPFLight/Extensions/EnumExtensions.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+namespace WPFLight.Extensions {
+    public static class EnumHelpers {
+        private static void CheckIsEnum<T> (bool withFlags) {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException(string.Format("Type '{0}' is not an enum", typeof(T).FullName));
+            if (withFlags && !Attribute.IsDefined(typeof(T), typeof(FlagsAttribute)))
+                throw new ArgumentException(string.Format("Type '{0}' doesn't have the 'Flags' attribute", typeof(T).FullName));
+        }
+
+        public static bool IsFlagSet<T> (this T value, T flag) where T : struct {
+            CheckIsEnum<T>(true);
+            long lValue = Convert.ToInt64(value);
+            long lFlag = Convert.ToInt64(flag);
+            return (lValue & lFlag) != 0;
+        }
+
+        public static IEnumerable<T> GetFlags<T> (this T value) where T : struct {
+            CheckIsEnum<T>(true);
+
+#if WINDOWS_PHONE
+            foreach (T flag in GetValues<T> ( )) {
+#else
+            foreach (T flag in Enum.GetValues(typeof(T)).Cast<T>()) {
+#endif
+                if (value.IsFlagSet(flag))
+                    yield return flag;
+            }
+        }
+
+        public static T SetFlags<T> (this T value, T flags, bool on) where T : struct {
+            CheckIsEnum<T>(true);
+            long lValue = Convert.ToInt64(value);
+            long lFlag = Convert.ToInt64(flags);
+            if (on) {
+                lValue |= lFlag;
+            } else {
+                lValue &= (~lFlag);
+            }
+            return (T)Enum.ToObject(typeof(T), lValue);
+        }
+
+        public static T SetFlags<T> (this T value, T flags) where T : struct {
+            return value.SetFlags(flags, true);
+        }
+
+        public static T ClearFlags<T> (this T value, T flags) where T : struct {
+            return value.SetFlags(flags, false);
+        }
+
+        public static T CombineFlags<T> (this IEnumerable<T> flags) where T : struct {
+            CheckIsEnum<T>(true);
+            long lValue = 0;
+            foreach (T flag in flags) {
+                long lFlag = Convert.ToInt64(flag);
+                lValue |= lFlag;
+            }
+            return (T)Enum.ToObject(typeof(T), lValue);
+        }
+
+#if WINDOWS_PHONE
+
+        /*
+         * Enum.GetValues doesn't exist in Windows Phone 7, so I need to add the same functionality
+         * */
+
+        public static T[] GetValues<T> () {
+            Type enumType = typeof(T);
+            if (!enumType.IsEnum)
+                throw new ArgumentException("Type '" + enumType.Name + "' is not an enum");
+
+            FieldInfo[] fields = enumType.GetFields();
+            int literalCount = 0;
+            for (int i = 0; i < fields.Length; i++)
+                if (fields[i].IsLiteral == true)
+                    literalCount++;
+
+            T[] arr = new T[literalCount];
+            int pos = 0;
+            for (int i = 0; i < fields.Length; i++)
+                if (fields[i].IsLiteral == true) {
+                    arr[pos] = (T)fields[i].GetValue(enumType);
+                    pos++;
+                }
+
+            return arr;
+        }
+#endif
+    }
+}
---
+++ WPFLight/System/Windows/FrameworkPropertyMetadata.cs
@@ -0,0 +1,194 @@
+using System.Collections.Generic;
+using System.Windows.Data;
+using WPFLight.Extensions;
+namespace System.Windows {
+	public class FrameworkPropertyMetadata : PropertyMetadata {
+		public FrameworkPropertyMetadata ( ) {
+
+        }
+
+		public FrameworkPropertyMetadata ( object defaultValue ) : base ( defaultValue ) { 
+        
+        }
+
+        public FrameworkPropertyMetadata (object defaultValue, FrameworkPropertyMetadataOptions options ) : base(defaultValue) {
+            SetOptions(options);
+        }
+
+        public FrameworkPropertyMetadata (object defaultValue, PropertyChangedCallback callback, FrameworkPropertyMetadataOptions options ) : base(defaultValue, callback) {
+            SetOptions(options);
+        }
+
+		public FrameworkPropertyMetadata (
+			object defaultValue, PropertyChangedCallback callback, FrameworkPropertyMetadataOptions options, UpdateSourceTrigger defaultUpdateSourceTrigger ) : this (defaultValue, callback, options) {
+			this.DefaultUpdateSourceTrigger = defaultUpdateSourceTrigger;
+        }
+		
+		public FrameworkPropertyMetadata ( object defaultValue, PropertyChangedCallback callback ) : base ( defaultValue, callback ) { }
+
+		public FrameworkPropertyMetadata ( PropertyChangedCallback callback ) : base ( callback ) { }
+
+        #region Properties
+
+        /// <summary>
+        ///    The default UpdateSourceTrigger for two-way data bindings on this property.
+        /// </summary>
+        public UpdateSourceTrigger DefaultUpdateSourceTrigger { get; set; }
+
+        /// <summary>
+        /// Property is inheritable
+        /// </summary>
+		public bool Inherits { get; set; }
+
+        /// <summary>
+        ///     Property affects measurement
+        /// </summary>
+        public bool AffectsMeasure { get; set; }
+
+        /// <summary>
+        ///     Property affects arragement
+        /// </summary>
+        public bool AffectsArrange {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Property affects parent's measurement
+        /// </summary>
+        public bool AffectsParentMeasure {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Property affects parent's arrangement
+        /// </summary>
+        public bool AffectsParentArrange {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Property affects rendering
+        /// </summary>
+        public bool AffectsRender {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Property evaluation must span separated trees
+        /// </summary>
+        public bool OverridesInheritanceBehavior {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        ///     Property cannot be data-bound
+        /// </summary>
+        public bool IsNotDataBindable {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     Data bindings on this property default to two-way
+        /// </summary>
+        public bool BindsTwoWayByDefault {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///    The default UpdateSourceTrigger for two-way data bindings on this property.
+        /// </summary>
+        public UpdateSourceTrigger DefaultUpdateSourceTrigger {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        ///     The value of this property should be saved/restored when journaling by URI
+        /// </summary>
+        public bool Journal {
+            get;
+            set;
+        }
+
+        /// <summary>
+        ///     This property's subproperties do not affect rendering.
+        ///     For instance, a property X may have a subproperty Y.
+        ///     Changing X.Y does not require rendering to be updated.
+        /// </summary>
+        public bool SubPropertiesDoNotAffectRender {
+            get;
+            set;
+        }
+ 
+        #endregion
+
+        void SetOptions (FrameworkPropertyMetadataOptions options) {
+            this.AffectsArrange = options.IsFlagSet(FrameworkPropertyMetadataOptions.AffectsArrange);
+            this.AffectsMeasure = options.IsFlagSet(FrameworkPropertyMetadataOptions.AffectsMeasure);
+            this.AffectsParentArrange = options.IsFlagSet(FrameworkPropertyMetadataOptions.AffectsParentArrange);
+            this.AffectsParentMeasure = options.IsFlagSet(FrameworkPropertyMetadataOptions.AffectsParentMeasure);
+            this.AffectsRender = options.IsFlagSet(FrameworkPropertyMetadataOptions.AffectsRender);
+            this.BindsTwoWayByDefault = options.IsFlagSet(FrameworkPropertyMetadataOptions.BindsTwoWayByDefault);
+            this.Inherits = options.IsFlagSet(FrameworkPropertyMetadataOptions.Inherits);
+            this.Journal = options.IsFlagSet(FrameworkPropertyMetadataOptions.Journal);
+            this.IsNotDataBindable = options.IsFlagSet(FrameworkPropertyMetadataOptions.NotDataBindable);
+            this.OverridesInheritanceBehavior = options.IsFlagSet(FrameworkPropertyMetadataOptions.OverridesInheritanceBehavior);
+            this.SubPropertiesDoNotAffectRender = options.IsFlagSet(FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender);
+        }
+    }
+
+    [Flags]
+    public enum FrameworkPropertyMetadataOptions : int {
+        /// <summary>No flags</summary>
+        None = 0x000,
+
+        /// <summary>This property affects measurement</summary>
+        AffectsMeasure = 0x001,
+
+        /// <summary>This property affects arragement</summary>
+        AffectsArrange = 0x002,
+
+        /// <summary>This property affects parent's measurement</summary>
+        AffectsParentMeasure = 0x004,
+
+        /// <summary>This property affects parent's arrangement</summary>
+        AffectsParentArrange = 0x008,
+
+        /// <summary>This property affects rendering</summary>
+        AffectsRender = 0x010,
+
+        /// <summary>This property inherits to children</summary>
+        Inherits = 0x020,
+
+        /// <summary>
+        /// This property causes inheritance and resource lookup to override values 
+        /// of InheritanceBehavior that may be set on any FE in the path of lookup
+        /// </summary>
+        OverridesInheritanceBehavior = 0x040,
+
+        /// <summary>This property does not support data binding</summary>
+        NotDataBindable = 0x080,
+
+        /// <summary>Data bindings on this property default to two-way</summary>
+        BindsTwoWayByDefault = 0x100,
+
+        /// <summary>This property should be saved/restored when journaling/navigating by URI</summary>
+        Journal = 0x400,
+
+        /// <summary>
+        ///     This property's subproperties do not affect rendering.
+        ///     For instance, a property X may have a subproperty Y.
+        ///     Changing X.Y does not require rendering to be updated.
+        /// </summary>
+        SubPropertiesDoNotAffectRender = 0x800,
+    }
+}
